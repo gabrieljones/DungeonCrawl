9ede71c52bf7844aed7c871752fd39dce13c9fc7
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2020-02-06 23:58:16 -0500

    inital commit

---
+++ DungeonCrawl.ino
@@ -0,0 +1,158 @@
+typedef void (*State) ();
+
+void Setup();
+void AvatarInit();
+void Avatar();
+void AvatarMovingInit();
+void AvatarMoving();
+void PathInit();
+void Path();
+void WallInit();
+void Wall();
+void ResetInit();
+void ResetIgnore();
+void Reset();
+
+State state = Setup;
+
+#define PATH_COLOR BLUE
+#define AVATAR_COLOR GREEN
+#define WALL_COLOR RED
+#define FOG_COLOR WHITE
+#define RESET_COLOR MAGENTA
+
+enum commandStates {NONE, AVATAR, MOVE, PATH, WALL, WALL_REVEALED, RESET}; //add a treasure tile?
+bool revealed = false;
+byte heading = 0;
+Timer timer;
+
+void Setup() {
+  if(startState() == START_STATE_WE_ARE_ROOT) {
+    state = AvatarInit;
+  } else {
+    state = random(1) ? PathInit : WallInit;
+  }
+}
+
+void AvatarInit() {
+  setValueSentOnAllFaces(AVATAR);
+  setColor(PATH_COLOR);
+  setColorOnFace(AVATAR_COLOR, heading);
+  revealed = true; //will become revealed path after adventurer leaves
+  state = Avatar;
+}
+
+void Avatar() {
+  if (buttonSingleClicked()) { //rotate right
+    byte prev = heading;
+    heading = (heading + 1) % 6;
+    setColorOnFace(AVATAR_COLOR, heading);
+    setColorOnFace(PATH_COLOR, prev);
+  }
+  if (buttonDoubleClicked() || buttonMultiClicked()) { //rotate left
+    byte prev = heading;
+    heading = (heading - 1) % 6;
+    setColorOnFace(AVATAR_COLOR, heading);
+    setColorOnFace(PATH_COLOR, prev);
+  }
+  if (buttonLongPressed()) {
+    state = AvatarMovingInit;
+  }
+}
+
+void AvatarMovingInit() {
+  setValueSentOnFace(MOVE, heading);
+  state = AvatarMoving;
+}
+
+void AvatarMoving() {
+  if (!isValueReceivedOnFaceExpired(heading)) {
+    switch(getLastValueReceivedOnFace(heading)) {
+      case AVATAR:
+        state = PathInit; //avatar moved onto next path, become an empty path tile
+        break;
+      case WALL_REVEALED:
+        state = AvatarInit; //wall has been revealed, avatar is still here
+        break;
+      case RESET: //we are only listening on one face here, but catch this in case a reset has happened somehow
+        state = ResetInit;
+        break;
+    }
+  }
+}
+
+void PathInit() {
+  setValueSentOnAllFaces(PATH);
+  setColor(revealed ? PATH_COLOR : FOG_COLOR);
+  state = Path;
+}
+
+void Path() {
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {
+      switch(getLastValueReceivedOnFace(heading)) {
+        case MOVE:
+          state = AvatarInit;
+          break;
+        case RESET:
+          state = ResetInit;
+          break;
+      }
+    }
+  }
+}
+
+void WallInit() {
+  setValueSentOnAllFaces(revealed ? WALL_REVEALED : WALL);
+  setColor(revealed ? WALL_COLOR : FOG_COLOR);
+  state = Wall;
+}
+
+void Wall() {
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f)) {
+      switch(getLastValueReceivedOnFace(heading)) {
+        case MOVE:
+          revealed = true;
+          state = WallInit;
+          break;
+        case RESET:
+          state = ResetInit;
+          break;
+      }
+    }
+  }
+  if (buttonLongPressed()) {
+    state = ResetInit;
+  }
+}
+
+void ResetInit() {
+  setValueSentOnAllFaces(RESET);
+  setColor(RESET_COLOR);
+  timer.set(512);
+  state = ResetIgnore;
+}
+
+//stop broadcasting reset after a bit, then ignore the reset broadcast
+void ResetIgnore() {
+  setValueSentOnAllFaces(NONE);
+  if (timer.isExpired()) {
+    timer.set(512);
+    state = Reset;
+  }
+}
+
+//ignore reset wave for a bit more, then reinitialize
+void Reset() {
+  if (timer.isExpired()) {
+    state = Setup;
+  }
+}
+
+void setup() {
+}
+
+void loop() {
+  state();
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Dungeon Crawl
+###### by Gabriel Jones
+  
+| Number of Blinks | Number of Players | Duration of Gameplay | Recommended Ages |
+|:----------------:|:-----------------:|:--------------------:|:----------------:|
+| 12+              | 1                 | ???                  | 8 & Up           |
+
+## Objective
+
+More of an experiment in a game mechanic than an actual game at the moment.
+
+To be determined.
+
+Try to visit all the tiles?
+
+## Setup
+
+Root tile becomes avatar.
+
+Other tiles become either hidden paths, or hidden walls.
+
+## Gameplay
+
+### Colors
+- Green: Avatar
+- Blue: Empty Path
+- White: Fog
+- Red: Walls
+
+### Avatar
+ - **Long-press**: moves avatar along current heading unless there is a wall.
+ - **Single-click**: turns avatar to the right.
+ - **Double-click**: turns avatar to the left.
+
+### Wall
+ - **Long-press**: reset connected tiles and generate a new dungeon.
