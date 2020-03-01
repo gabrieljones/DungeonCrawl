77d7ab618dc2fa68d4d698caa3f0a17776a4a87b
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2020-02-29 21:57:34 -0500

    Labyrinth: everything but the actual avatar moving.

--- DungeonCrawl.ino
+++ DungeonCrawl.ino
@@ -6,80 +6,225 @@
 #define FOG_COLOR WHITE
 #define RESET_COLOR MAGENTA
 #define STAIRS_COLOR YELLOW
+//#define GAME_TIME_MAX 360000 //6 minutes
+#define GAME_TIME_MAX 10000 //10 seconds
+#define LEVEL_MAX 6
 
-enum tileTypes {NONE, AVATAR, FOG, PATH, WALL, STAIRS, MOVE}; //add a treasure tile?
+enum tileTypes {NONE, FOG, PATH, WALL, STAIRS, MOVE, BECOME_PATH, BECOME_WALL, BECOME_STAIRS, BECOME_FOG, ASCEND, WIN, LOSE}; //add a treasure tile?
+byte gameOverState = NONE;
 byte heading = 0;
 Timer timer;
+unsigned long startMillis;
+byte level = 6;
+byte neighbors[6];
 
+STATE_DEC(setupAvatarS);
 STATE_DEC(avatarS);
-STATE_DEC(avatarMovingS);
+STATE_DEC(avatarEnteringS);
+STATE_DEC(avatarLeavingS);
+STATE_DEC(avatarLeftS);
+STATE_DEC(avatarAscendingS);
+STATE_DEC(avatarAscendedS);
 STATE_DEC(fogS);
 STATE_DEC(pathS);
 STATE_DEC(stairsS);
 STATE_DEC(wallS);
+STATE_DEC(gameOverBroadcastS);
+STATE_DEC(gameOverIgnoreS);
+STATE_DEC(gameOverAvatarS);
 
-bool noPathToAvatar() {
+void conformToAvatarMap() {
   FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) != NONE) {
-      return false;
+    if (!isValueReceivedOnFaceExpired(f)) {
+      switch(getLastValueReceivedOnFace(f)) {
+        case BECOME_FOG:
+          changeState(fogS::state);
+          break;
+        case BECOME_WALL:
+          changeState(wallS::state);
+          break;
+        case BECOME_PATH:
+          changeState(pathS::state);
+          break;
+        case BECOME_STAIRS:
+          changeState(stairsS::state);
+          break;
+        case WIN:
+          gameOverState = WIN;
+          changeState(gameOverBroadcastS::state);
+          break;
+        case LOSE:
+          gameOverState = LOSE;
+          changeState(gameOverBroadcastS::state);
+          break;
+      }
     }
   }
-  return true;
 }
 
-STATE_DEF(setupS,
+bool receiveAvatar(byte entranceFace, byte* level, int* millisRemaining) {
+      //look for the avatarDatagram
+  if (isDatagramReadyOnFace(entranceFace)) {//is there a packet?
+    if (getDatagramLengthOnFace(entranceFace) == 3) {//is it the right length?
+      byte *data = (byte *) getDatagramOnFace(entranceFace);//grab the data
+      byte currentLevel = data[0];
+//      byte millisRemaining = data[1];
+      markDatagramReadOnFace(entranceFace); // free datagram buffer
+      return true;
+    }
+  }
+  return false;
+}
+
+void sendAvatar(byte exitFace, byte level, int millisRemaining) {
+  byte data[3];  // level and millisRemaining
+  data[0] = level;
+  data[1] = millisRemaining;
+//  data[2] = currentCarHue;
+  sendDatagramOnFace(&data, sizeof(data), exitFace);
+}
+
+STATE_DEF(setupAvatarS,
   { //entry
     randomize();
+    startMillis = millis();
+    level = 0;
   },
   { //loop
-    changeState(startState() == START_STATE_WE_ARE_ROOT ? avatarS::state : fogS::state);
+    changeState(avatarS::state);
   }
 )
 
 STATE_DEF(avatarS, 
   { //entry
-    //TODO set faces telling neighbors type of tile they are
-    FOREACH_FACE(f) {
-      setValueSentOnFace(f, f); //debug 
-    }
+    byte wallCount = 0;
+    bool stairsAdj = false;
     setColor(AVATAR_COLOR);
+    FOREACH_FACE(f) { //accept existing tiles
+      if (!isValueReceivedOnFaceExpired(f)) {
+        neighbors[f] = getLastValueReceivedOnFace(f);
+        switch(neighbors[f]) {
+          case WALL:
+            wallCount++;
+            break;
+          case STAIRS:
+            stairsAdj = true;
+            break;
+        }
+      }
+    }
+    FOREACH_FACE(f) {// assign all the faces keeping existing neighbors
+      switch(neighbors[f]) {
+        case PATH:
+          setValueSentOnFace(BECOME_PATH, f);
+          break;
+        case STAIRS:
+          setValueSentOnFace(BECOME_STAIRS, f);
+          break;
+        case WALL:
+          setValueSentOnFace(BECOME_WALL, f);
+          break;
+        case NONE:
+        case FOG:
+          byte newNeighbor = wallCount < 4 && random(2) == 0 ? BECOME_WALL : ( stairsAdj || random(9) > 0  ? BECOME_PATH : BECOME_STAIRS );
+          //^^ if wallCount < 4 then 1 in 3 chance for WALL else if stairsAdj or 2 in 3 * 9 in 10 chance for PATH else 2 in 3 * 1 in 10 for STAIRS
+          switch(newNeighbor) {
+            case BECOME_WALL:
+              wallCount++;
+              break;
+            case BECOME_STAIRS:
+              stairsAdj = true;
+              break;
+          }
+          setValueSentOnFace(newNeighbor, f);
+          break;
+      }
+    }
   },
   { //loop
     FOREACH_FACE(f) {
       if (!isValueReceivedOnFaceExpired(f)) {
         switch(getLastValueReceivedOnFace(f)) {
-          case MOVE: //avatar is being pulled to neighbor revert to path
-            setValueSentOnFace(NONE, f); //inform requesting tile that the avatar has left this tile to live on the requesting tile
-            changeState(pathS::state);
+          case MOVE: //avatar is being pulled to neighbor
+            heading = f;
+            changeState(avatarLeavingS::state);
             break;
         }
+      }
+    }
+    if (millis() - startMillis > GAME_TIME_MAX) {
+      gameOverState = LOSE;
+      changeState(gameOverAvatarS::state);
+    }
+    if (buttonLongPressed()) changeState(fogS::state);
+    //animate time remaining    
+    setColor(AVATAR_COLOR);
+    byte blinkFace = (millis() - startMillis) / (GAME_TIME_MAX / 6);
+    Color  on = AVATAR_COLOR;
+    Color off = dim(AVATAR_COLOR,  32);
+    Color blinq = millis() % 1000 / 500 == 0 ? off : on;
+    FOREACH_FACE(f) {
+           if (f < blinkFace) setColorOnFace(  off, f);
+      else if (f > blinkFace) setColorOnFace(   on, f);
+      else                    setColorOnFace(blinq, f);
+    }
+  }
+)
+
+STATE_DEF(avatarLeavingS, 
+  { //entry
+    setColorOnFace(AVATAR_COLOR, heading);
+    setColorOnFace(AVATAR_COLOR, (heading + 1) % 6);
+    setColorOnFace(AVATAR_COLOR, (heading + 5) % 6);
+  },
+  { //loop
+    sendAvatar(heading, level, millis() - startMillis);
+    changeState(avatarLeftS::state);
+    
+  }
+)
+
+STATE_DEF(avatarLeftS, 
+  { //entry
+    timer.set(150); //timeout for sending avatar datagram
+  },
+  { //loop
+    if (timer.isExpired()) { //avatar datagram was not acknowledged within timeout
+      changeState(avatarS::state); //revert back to avatar
+    }
+    if (!isValueReceivedOnFaceExpired(heading)) {
+      switch(getLastValueReceivedOnFace(heading)) {
+        case BECOME_PATH: //avatar moved to path
+          changeState(pathS::state);
+          break;
+        case BECOME_FOG: //avatar ascended stairs
+          changeState(fogS::state);
+          break;
       }
     }
   }
 )
 
-STATE_DEF(avatarMovingS, 
+STATE_DEF(avatarEnteringS, 
   { //entry
     setValueSentOnFace(MOVE, heading); //request the avatar move here
     setColorOnFace(AVATAR_COLOR, heading);
     setColorOnFace(AVATAR_COLOR, (heading + 1) % 6);
     setColorOnFace(AVATAR_COLOR, (heading + 5) % 6);
-    timer.set(512); //if the avatar does not move here, then it must not be on the neighbor
   },
   { //loop
+    int millisRemaining;
+    receiveAvatar(heading, &level, &millisRemaining);
+    
     bool doneMoving = true;
     FOREACH_FACE(f) {
       if (!isValueReceivedOnFaceExpired(f)) {
         switch(getLastValueReceivedOnFace(f)) {
-          case PATH: //wait for all neighbors to be not PATHs, this means the AVATAR has moved to us
+          case BECOME_PATH: //wait for all neighbors to be not BECOME_PATHs, this means the AVATAR has moved to us
             doneMoving = false;
             break;
         }
       }
-    }
-    if (timer.isExpired()) { //avatar did not move here revert back to empty path
-      setValueSentOnFace(NONE, heading); //stop requesting avatar moves here
-      changeState(pathS::state);
     }
     if (doneMoving) { //after avatar is confirmed to be here then transition to actual Avatar state
       changeState(avatarS::state);
@@ -87,33 +232,73 @@ STATE_DEF(avatarMovingS,
   }
 )
 
-STATE_DEF(fogS, 
+STATE_DEF(avatarAscendingS, 
   { //entry
-    setValueSentOnAllFaces(NONE);
-    setColor(FOG_COLOR);
+    setValueSentOnFace(MOVE, heading); //request the avatar move here
+    setColorOnFace(AVATAR_COLOR, heading);
+    setColorOnFace(AVATAR_COLOR, (heading + 1) % 6);
+    setColorOnFace(AVATAR_COLOR, (heading + 5) % 6);
   },
   { //loop
+    bool doneMoving = true;
     FOREACH_FACE(f) {
       if (!isValueReceivedOnFaceExpired(f)) {
         switch(getLastValueReceivedOnFace(f)) {
-          case WALL:
-            changeState(wallS::state);
-            break;
-          case PATH:
-            changeState(pathS::state);
-            break;
-          case STAIRS:
-            changeState(stairsS::state);
+          case BECOME_STAIRS: //wait for all neighbors to be not BECOME_STAIRSs, this means the AVATAR has moved to us
+            doneMoving = false;
             break;
         }
       }
     }
+    if (doneMoving) { //after avatar is confirmed to be here then transition to actual ascended state
+      changeState(avatarAscendedS::state);
+    }
+  }
+)
+
+STATE_DEF(avatarAscendedS, 
+  { //entry
+    level++;
+    setValueSentOnAllFaces(BECOME_FOG);
+  },
+  { //loop
+    if (level >= LEVEL_MAX) {//we won
+      gameOverState = WIN;
+      changeState(gameOverAvatarS::state);
+      return;
+    }
+    bool doneAscending = true;
+    FOREACH_FACE(f) {
+      if (!isValueReceivedOnFaceExpired(f)) {
+        switch(getLastValueReceivedOnFace(f)) {
+          case FOG: //wait for all neighbors to be FOG, this means the AVATAR has ascended
+            break;
+          default:
+            doneAscending = false;
+            break;
+        }
+      }
+    }
+    if (doneAscending) { //after avatar is confirmed to have ascended then transition to actual Avatar state
+      changeState(avatarS::state);
+    }
+  }
+)
+
+STATE_DEF(fogS, 
+  { //entry
+    setValueSentOnAllFaces(FOG);
+    setColor(FOG_COLOR);
+  },
+  { //loop
+    conformToAvatarMap();
+    if (buttonLongPressed()) changeState(setupAvatarS::state);
   }
 )
 
 STATE_DEF(pathS, 
   { //entry
-//leave faces as they are    setValueSentOnAllFaces(PATH); 
+    setValueSentOnAllFaces(PATH);
     setColor(PATH_COLOR);
   },
   { //loop
@@ -121,22 +306,23 @@ STATE_DEF(pathS,
       FOREACH_FACE(f) { //check if avatar is on neighbor
         if (!isValueReceivedOnFaceExpired(f)) {
           switch(getLastValueReceivedOnFace(f)) {
-            case PATH: //a neighbor is telling us we are a path, it might be the avatar, attempt to pull avatar
+            case BECOME_PATH: //a neighbor is telling us we are a path, its the avatar, attempt to pull avatar
               heading = f;
-              changeState(avatarMovingS::state);
+              changeState(avatarEnteringS::state);
               break;
           }
         }
       }
     }
-    if(noPathToAvatar()) { changeState(fogS::state); }
+    if(isAlone()) { changeState(fogS::state); return; }
+    conformToAvatarMap();
   }
 )
 
 
 STATE_DEF(stairsS, 
   { //entry
-//leave faces as they are    setValueSentOnAllFaces(PATH); 
+    setValueSentOnAllFaces(STAIRS);
     FOREACH_FACE(f) { 
       setColorOnFace(dim(STAIRS_COLOR, f * (255 / 6)), f);
     }
@@ -146,37 +332,91 @@ STATE_DEF(stairsS,
       FOREACH_FACE(f) { //check if avatar is on neighbor
         if (!isValueReceivedOnFaceExpired(f)) {
           switch(getLastValueReceivedOnFace(f)) {
-            case STAIRS: //a neighbor is telling us we are stairs, it might be the avatar, attempt to pull avatar
+            case BECOME_STAIRS: //a neighbor is telling us we are stairs, it might be the avatar, attempt to pull avatar
               heading = f;
-              changeState(avatarMovingS::state);
+              changeState(avatarAscendingS::state);
               break;
           }
         }
       }
     }
-    if(noPathToAvatar()) { changeState(fogS::state); }   
+    if(isAlone()) { changeState(fogS::state); return; }
+    conformToAvatarMap();
   }
 )
 
 STATE_DEF(wallS, 
   { //entry
-//leave faces as they are    setValueSentOnAllFaces(NONE);
+    setValueSentOnAllFaces(WALL);
     setColor(WALL_COLOR);
   },
   { //loop
-    if(noPathToAvatar()) { changeState(fogS::state); }
+    if(isAlone()) { changeState(fogS::state); return; }
+    conformToAvatarMap();
+  }
+)
+
+STATE_DEF(gameOverBroadcastS, 
+  { //entry
+    timer.set(500);
+    setValueSentOnAllFaces(gameOverState);
+    switch(gameOverState) {
+      case WIN: //TODO better win celebration animation
+        FOREACH_FACE(f) { 
+          setColorOnFace(dim(STAIRS_COLOR, f * (255 / 6)), f);
+        }
+        break;
+      case LOSE:
+        setColor(WALL_COLOR);
+        FOREACH_FACE(f) {
+          if (f % 2 == 0) setColorOnFace(OFF, f);
+        }
+        break;
+    }
+  },
+  { //loop
+    if(timer.isExpired()) {
+      changeState(gameOverIgnoreS::state);
+    }
+  }
+)
+
+STATE_DEF(gameOverIgnoreS,
+  { //entry
+    timer.set(500);
+    setValueSentOnAllFaces(NONE); //stop broadcasting gameOver wave
+  },
+  { //loop
+    if(timer.isExpired()) { //stop ignoring
+      FOREACH_FACE(f) {
+        if (!isValueReceivedOnFaceExpired(f) && getLastValueReceivedOnFace(f) != NONE) { //a neighbor is broadcasting a new state this must be a new game
+          changeState(fogS::state);
+        }
+      }
+    }
+    if (buttonSingleClicked()) changeState(fogS::state);
+  }
+)
+
+STATE_DEF(gameOverAvatarS, 
+  { //entry
+    timer.set(500);
+    setValueSentOnAllFaces(gameOverState);
+    setColor(AVATAR_COLOR);
+  },
+  { //loop
+    if(timer.isExpired()) {
+      setValueSentOnAllFaces(NONE); //stop broadcasting gameOver wave
+    }
+    if (buttonSingleClicked()) changeState(setupAvatarS::state);
+    if (buttonLongPressed()) changeState(fogS::state);
   }
 )
 
 void setup() {
-  changeState(setupS::state);
+  changeState(fogS::state);
 }
 
 void loop() {
   stateFn();
 }
-
-/*
- * 3422, 700
- * /
- */
--- README.md
+++ README.md
@@ -1,40 +1,59 @@
-# Dungeon Crawl
+# Labyrinth
 ###### by Gabriel Jones
   
 | Number of Blinks | Number of Players | Duration of Gameplay | Recommended Ages |
 |:----------------:|:-----------------:|:--------------------:|:----------------:|
-| 12+              | 1                 | ???                  | 8 & Up           |
+| 6+              | 1                 | 6 minutes             | 8 & Up           |
 
 ## Objective
 
-More of an experiment in a game mechanic than an actual game at the moment.
-
-To be determined.
-
-Try to visit all the tiles?
+You have been imprisoned within the labyrinth. You must escape before you die of starvation (6 minutes).
+You are on the sixth sublevel of the labyrinth you must search for the stairs to the levels above.
+Reach the stairs to the outside world and you shall gain your freedom.
+Beware, areas of the labyrinth that are out of sight tend to change.  
 
 ## Setup
 
-Root tile becomes avatar.
-
-Other tiles become either hidden paths, or hidden walls.
+All tiles start as Fog tiles.
+Long press a Fog tile to change it to the avatar and begin the timer.
 
 ## Gameplay
+
+- Place Fog tiles next to the avatar to reveal the adjacent space around the avatar.
+- Click on Path tiles adjacent to the avatar to move the avatar to that tile.
+- Click on Stairs tiles adjacent to the avatar to ascend to the level above.
+- Remove tiles from the board to create new fog tiles that can be placed next the avatar to reveal as yet unexplored areas of the labyrinth.
+- If you escape the labyrinth within the allotted time you escape (WIN).
+- If you fail to find the sixth and final stairs to the surface before time is up you starve and are imprisoned within the labyrinth forever (LOSE).
 
 ### Colors
 - Green: Avatar
 - Blue: Empty Path
 - White: Fog
 - Red: Walls
+- Yellow: Stairs to the next level above
 
-### Fog Tile Next to Avatar
- - **Single-click**: Reveal tile and move avatar to tile if it is a path.
+### Fog Tile
+ - **Long-press**: Become Avatar and start timer.
+ - **Place next to Avatar**: Reveal that tile.
+ - **Place next to other tile**: Stay Fog.
+
+### Avatar Tile
+ - **Long-press**: Become Fog.
 
 ### Path Tile Next to Avatar
  - **Single-click**: Move avatar to tile.
 
+### Stairs Tile Next to Avatar
+ - **Single-click**: Move avatar to tile and ascend to next level above.
+
 ### Wall Tile Next to Avatar
  - **Single-click**: Nothing happens.
+ 
+### Game Over Avatar Tile
+ - **Single-click**: Start new Labyrinth.
+ - **Long-press**: Become Fog.
+ 
+### Game Over Tile
+ - **Single-click**: Revert all connected tiles to Fog.
 
-### Any tile
- - **Long-press**: reset connected tiles and generate a new dungeon.
